919b6d9a72493d4eb27f71efd69b5c7d50dcaf0e
Akashp132 <akashpnaircpd2000@gmail.com>
2024-09-07 19:08:18 +0530

    2

--- week1-tasks/CMS.py
+++ week1-tasks/CMS.py
@@ -10,7 +10,7 @@ class Contact:
         self.phone = phone
         self.email = email
     def __str__(self):
-        return f"Name:{self.name},Phone Number:{self.phone},Email:{self.email}"
+        return f"Name:{self.name}\nPhone Number:{self.phone}\nEmail:{self.email}"
     
 
 #functions
@@ -41,7 +41,7 @@ class contactsFunction:
 
     #validation (name,email and phone number)
     def validateName(self,name):
-        pattern=r'^[a-zA-z]'
+        pattern=r'^[a-zA-Z]+$'
         return re.match(pattern,name) is not None
     def validateEmail(self,email):
         pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
@@ -64,13 +64,13 @@ class contactsFunction:
     #adding contact function
     def addContacts(self,name,phone,email):
         if not self.validateName(name):
-            print("Please enter alaphabets only!")
+            print("Please enter alaphabets only in Name!")
             return
         if not self.validateEmail(email):
             print("Invalid email format!")
             return
         if not self.validatePhoneno(phone):
-            print("Invalid phone number! It should be 10 digits")
+            print("Invalid phone number! It should be 10 digits only")
             return
         if name in self.contacts:
             print("Contact already exits!!")
@@ -80,12 +80,18 @@ class contactsFunction:
         print("Contact added successfully")
 
     #search function
-    def searchContact(self,name):
-        if name in self.contacts:
-            print(self.contacts[name])
-        else:
+    def searchContact(self, name):
+        status = False
+        searchName = name.lower()
+        print(f"Searching for: {searchName}")
+        for contactName in self.contacts:
+            if searchName in contactName.lower():
+                print(self.contacts[contactName])
+                status = True
+        if not status:
             print(f"Contact {name} not found!!")
-
+    
+                
     #update function
     def updateContact(self,name,phone,email):
         if not self.validateEmail(email):
@@ -105,7 +111,7 @@ class contactsFunction:
     #delete function
     def deleteContact(self,name):
         if name in self.contacts:
-            status=input("Are you sure You want to delete the contact'{name}'?(Yes/No):").strip().lower()
+            status=input(f"Are you sure You want to delete the contact '{name}' ? (Yes/No):").strip().lower()
             if status=='yes':
                 del self.contacts[name]
                 self.saveContacts()
@@ -128,10 +134,10 @@ def main():
         print("\n4. Update Contact")
         print("\n5. Delete Contact")
         print("\n6. Exit")
-        choice=input("Enter your choice: ").strip()
+        choice=input("\nEnter your choice: ").strip()
 
         if choice =='1':
-            print(cms.showContacts())
+            cms.showContacts()
         elif choice=='2':
             name=input("Enter name: ").strip()
             phone=input("Enter phone number: ").strip()
@@ -149,6 +155,7 @@ def main():
             name=input("Enter name to delete: ").strip()
             cms.deleteContact(name)
         elif choice=='6':
+            print("Exiting CMS, Goodbye!")
             break
         else:
             print("Invalid choice!try again")
